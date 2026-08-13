c56341c6fe093cff463a5742e1ff30f87cdf3b28
Matias Grinberg <maticgrinberg@gmail.com>
2026-08-13 02:35:19 -0300

    feat(mock-gateway): step-up middleware and audit log infrastructure

---
+++ tools/mock-gateway/src/middleware/stepUp.js
@@ -0,0 +1,19 @@
+const { sendError } = require('../errors');
+
+function requireStepUp(req, res, next) {
+  const code = req.headers['x-ops-totp'];
+  if (!code) {
+    return sendError(
+      res,
+      403,
+      'step_up_required',
+      'Esta acción requiere el código TOTP en el header X-Ops-Totp'
+    );
+  }
+  if (code !== '000000') {
+    return sendError(res, 403, 'invalid_totp', 'Código TOTP inválido');
+  }
+  next();
+}
+
+module.exports = { requireStepUp };
