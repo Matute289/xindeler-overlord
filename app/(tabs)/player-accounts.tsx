226654c489b2acd309e8f85a9bfe3115f4d3c93b
Matias Grinberg <maticgrinberg@gmail.com>
2026-08-15 17:37:59 -0300

    feat(oc52): player 2FA unlock screen, wired into Más

---
+++ app/(tabs)/player-accounts.tsx
@@ -0,0 +1,10 @@
+import { PlayerAccountsScreen } from '@/features/playerAccounts/PlayerAccountsScreen';
+import { Screen } from '@/ui/Screen';
+
+export default function PlayerAccountsRoute() {
+  return (
+    <Screen>
+      <PlayerAccountsScreen />
+    </Screen>
+  );
+}
